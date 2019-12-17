d7b1d14a9bc86751fc2cfd70ae40d42f3e612ab7
Regino P <regino5298@gmail.com>
2019-12-17 16:46:29 -0700

    Met MVP

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
+import { Reset } from 'styled-reset';
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+
+const PrivateRouter = ({ compnent: Component, ...rest}) => (
+
+  <Route
+    {...rest}
+    render ={props => 
+    localStorage.getItem('token') ? (
+      <Component {...props} /> 
+    ) : (
+      <Redirect to="/" />
+    )
+    }
+    />
+);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Reset />
+      <Route exact path='/' component={Login} />
+      <PrivateRouter path="/friendsList" component={FriendsList} />
     </div>
   );
 }
